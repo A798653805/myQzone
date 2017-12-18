8b1ec6fefd695b31459676557032f2fd9741738f
xiaoFang <798653805@qq.com>
2017-12-18 11:23:32 +0800

    博客基本样式

--- src/pages/message/message.vue
+++ src/pages/message/message.vue
@@ -1,5 +1,93 @@
 <template>
-  <div>
-      我是博客页面
+  <div class="blog-box">
+    <div class="title-box">
+      <div class="title">
+        博客
+      </div>
+    </div>
+    <div class="search-box">
+      <el-input class="search" clearable size="mini" placeholder="输入要搜索的博客"></el-input>
+    </div>
+    <div class="blog-list">
+      <ul>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li> 
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+          <div class="menu"></div>
+        </li>
+         <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+         <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+         <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+         <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+        <li class="blog-item">
+          <div class="name">你是大傻子</div>
+          <div class="date">10月10日</div>
+        </li>
+      </ul>
+    </div>
+    <div class="block">
+      <el-pagination layout="prev, pager, next" page-size="15" :total="50">
+      </el-pagination>
+    </div>
   </div>
 </template>
+
+<style lang="less">
+  @import url('./less/message.less');
+
+</style>
+
+<script>
+  import {
+    blog
+  } from './message'
+  export default blog;
+
+</script>
