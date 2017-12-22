e2f710309f7be6619174e20d98074123735da3a3
xiaoFang <798653805@qq.com>
2017-12-22 17:10:56 +0800

    登陆和注册框重构

---
+++ src/pages/registerInput/registerInput.js
@@ -0,0 +1,135 @@
+export default {
+  // 绑定数据
+  data() {
+    //验证两次面是否相同
+    let validatePass = (rule, value, callback) => {
+      if (value !== this.registerForm.password) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    };
+    //验证账号是否存在
+    let validateUname = (rule, value, callback) => {
+      this.axios.post('/api/users/uname', {
+        username: this.registerForm.username
+      }).then((res) => {
+
+        if (res.flag) {
+          callback(new Error('用户已存在'));
+        } else {
+          callback();
+        }
+      }).catch((err) => {
+        console.log(err);
+        return false;
+      })
+    }
+    return {
+      registerForm: {
+        username: '',
+        password: '',
+        repassword: ''
+      },
+      rules: {
+        username: [{
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 14,
+            message: '长度在 6 到 14 个字符',
+            trigger: 'blur'
+          },
+          {
+            pattern: /^[a-zA-Z0-9_-]{6,14}$/,
+            message: '请使用字母，数字，下划线',
+            trigger: 'blur'
+          },
+          {
+            validator: validateUname,
+            trigger: 'blur'
+          }
+        ],
+        password: [{
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 14,
+            message: '长度在 6 到 14 个字符',
+            trigger: 'blur'
+          },
+          {
+            pattern: /^[a-zA-Z0-9_-]{6,14}$/,
+            message: '请使用字母，数字，下划线',
+            trigger: 'blur'
+          }
+        ],
+        repassword: [{
+            required: true,
+            message: '请再次输入密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 14,
+            message: '长度在 6 到 14 个字符',
+            trigger: 'blur'
+          },
+          {
+            pattern: /^[a-zA-Z0-9_-]{6,14}$/,
+            message: '请使用字母，数字，下划线',
+            trigger: 'blur'
+          },
+          {
+            validator: validatePass,
+            trigger: 'blur'
+          }
+        ]
+      }
+    };
+  },
+
+  // 方法
+  methods: {
+    //返回登录页
+    returnLogin() {
+      window.location.href = 'http://localhost:8080/#/login';
+    },
+    //发送数据
+    register() {
+      this.axios.post('/api/users/register', {
+          username: this.registerForm.username,
+          password: this.registerForm.password,
+        })
+        .then((response) => {
+          this.$message({
+            message: '注册成功,3秒钟后自动返回登录页面',
+            type: 'success'
+          });
+          setTimeout(() => {
+            this.returnLogin();
+          }, 3000);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    //提交数据
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.register();
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  },
+}
--- src/pages/registerInput/registerInput.vue
+++ src/pages/registerInput/registerInput.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="content">
-    <div class="header">
-      <h1>
-        <i class="iconfont icon-qzone"></i>个人空间</h1>
-    </div>
-    <div class="register-box">
-      <div class="register-header">
-        <div class="remind">
-          <h2>注册</h2>
-        </div>
-      </div>
-      <div class="under-line"></div>
-      <div class="register-body">
-        <el-form :model="registerForm" labelWidth="80px" ref="rulesForm" :rules="rules" label-position="left" class="form">
+  <div class="register-body">
+        <el-form :model="registerForm"  labelWidth="80px" ref="rulesForm" :rules="rules" label-position="left" class="form">
           <el-form-item label="用户名" prop="username">
             <el-input v-model="registerForm.username" class="input" size="small" placeholder="请输入用户名" id="username"></el-input>
           </el-form-item>
@@ -28,21 +16,16 @@
           </el-form-item>
         </el-form>
       </div>
-    </div>
-    <div class="footer">
-    </div>
-  </div>
 </template>
 
 <style lang="less">
-  @import url("./less/register");
-
+  @import url('./registerInput.less');
 </style>
 
 <script>
-  import {
-    register
-  } from './register';
-  export default register;
+  export {
+    default
+  }
+  from './registerInput.js'
 
 </script>
