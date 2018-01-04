97b3b8b68ff2d7aab1b602d7845426dc66ddf905
a798653805 <798653805@qq.com>
2018-01-04 19:48:47 +0800

    BUG

--- src/pages/login/loginWrap/loginWrap.vue
+++ src/pages/login/loginWrap/loginWrap.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="content">
+  <div class="content">
     <div class="header">
-      <h1><i class="iconfont icon-kongjian"></i>个人空间</h1>
+      <h1>
+        <i class="iconfont icon-kongjian"></i>个人空间</h1>
     </div>
     <slot></slot>
     <div class="footer">
     </div>
   </div>
 </template>
+
+<style lang="less">
+@import url('./loginWrap.less');
+</style>
